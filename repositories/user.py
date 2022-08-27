da5ffd20ec68a6bc6012605161a45bacf8c5cc1e
LamequeFernandes <lamequesao@gmail.com>
2022-08-27 15:01:59 -0300

    adicao no repository de user

--- repositories/user.py
+++ repositories/user.py
@@ -1,5 +1,4 @@
 from typing import List
-from unittest import result
 
 from models.user_model import UserModel
 
@@ -8,7 +7,6 @@ from fastapi import HTTPException, status
 from schemas.user_schema import UserSchema
 
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from sqlalchemy.future import select
 
 
@@ -31,9 +29,9 @@ class UserRepository:
     async def list_users(db: AsyncSession):
         async with db as session:
             try:
-                query = select(UserModel)
-                result = await session.execute(query)
-                users = result.scalars().all()
+                query_users = select(UserModel).order_by(UserModel.id)
+                result = await session.execute(query_users)
+                users: List[UserModel] = result.scalars().all()
             except Exception as erro:
                 raise HTTPException(
                         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
@@ -44,4 +42,55 @@ class UserRepository:
                         detail='Nenhum usuário encontrado')
             return users
     
+    @staticmethod
+    async def show(id: int, db: AsyncSession):
+        async with db as session:
+            try:
+                query_user = select(UserModel).filter(UserModel.id == id)
+                result_query = await session.execute(query_user)
+                user: UserModel = result_query.scalar()
+            except Exception as error:
+                raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                        detail=error)
+            if not user:
+                raise HTTPException(detail='Usuario não encontrado', 
+                                    status_code=status.HTTP_404_NOT_FOUND)
 
+            return user
+
+    @staticmethod
+    async def delete(id: int, db: AsyncSession):
+        async with db as session:
+            query_user = select(UserModel).filter(UserModel.id == id)
+            result_query = await session.execute(query_user)
+            user_delete = result_query.scalar()
+
+            if not user_delete: 
+                raise HTTPException(detail='Usuario não encontrado', 
+                                    status_code=status.HTTP_404_NOT_FOUND)
+            
+            await session.delete(user_delete)
+            await session.commit()
+
+            return dict(message = "Usuario deletado com sucesso")
+
+    @staticmethod
+    async def alter(id: int, body: UserSchema, db: AsyncSession):
+        async with db as session:
+            query_user = select(UserModel).filter(UserModel.id == id)
+            result_query = await session.execute(query_user)
+            user: UserModel = result_query.scalar()
+
+            if user:
+                body = body.dict()
+
+                for key in body:
+                    if body[key] != None:
+                        setattr(user, key, body[key])
+
+                await session.commit()
+                return user
+
+            raise HTTPException(detail='Usuario não encontrado', 
+                                status_code=status.HTTP_404_NOT_FOUND)
