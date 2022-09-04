3db90c38b00b33ab13ec44e1edac734aa034260b
LamequeFernandes <lamequesao@gmail.com>
2022-09-04 00:46:52 -0300

    alteracoes nos endpoints, refatorar dps

--- api/endpoints/renda.py
+++ api/endpoints/renda.py
@@ -3,11 +3,12 @@ from typing import List
 from fastapi import APIRouter, status, Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from models.user_model import UserModel
 
 from schemas.renda_schema import RendaSchema, RendaSchemaUp
 
 from core.database import get_session
-# from core.deps import get_current_user
+from core.deps import get_current_user
 from repositories.renda_repository import RendaRepository
 
 
@@ -15,9 +16,13 @@ router = APIRouter()
 
 renda_repository = RendaRepository()
 
-@router.post('/', response_model=RendaSchema)
-async def post_renda(renda: RendaSchema, db: AsyncSession = Depends(get_session)):
-    return await renda_repository.insert(renda, db)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=RendaSchema)
+async def post_renda(
+    renda: RendaSchema, 
+    user_logged: UserModel = Depends(get_current_user), 
+    db: AsyncSession = Depends(get_session)
+):
+    return await renda_repository.insert(renda, user_logged.id, db)
 
 
 @router.get('/{id}', response_model=RendaSchema)
--- api/endpoints/user.py
+++ api/endpoints/user.py
@@ -1,13 +1,18 @@
-from typing import List
+from typing import List, Optional, Any
 
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException, Response
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from schemas.user_schema import UserSchemaBase, UserSchemaCreate, UserSchemaUp
-
+from models.user_model import UserModel
 from core.database import get_session
-# from core.deps import get_current_user
+from core.deps import get_current_user
+# from core.security import checked_password, generate_hash_password
+from core.auth import authenticate, create_token_acess
+
 from repositories.user_repository import UserRepository
 
 
@@ -15,10 +20,43 @@ router = APIRouter()
 
 user_repository = UserRepository()
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=UserSchemaCreate)
-async def creater_user(user: UserSchemaCreate, db: AsyncSession = Depends(get_session)):
-    result = await user_repository.create(user=user, db=db)
-    return result
+# ---------------------------------------------------------
+
+@router.get('/logado', response_model=UserSchemaBase)
+def get_logado(usuario_logado: UserModel = Depends(get_current_user)):
+    return usuario_logado
+
+
+# signUp
+@router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=UserSchemaBase)
+async def post_usuario_jwt(
+    user: UserSchemaCreate, 
+    db: AsyncSession = Depends(get_session)
+):
+    return await user_repository.create(user, db)
+
+
+@router.post('/login')
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), 
+    db: AsyncSession = Depends(get_session)
+):
+    user = await authenticate(
+        email=form_data.username, 
+        password=form_data.password, 
+        db=db
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail='Dados de acesso incorretos.'
+        )
+    return JSONResponse(
+        content=dict(acess_token=create_token_acess(sub=user.id), token_type="bearer"), 
+        status_code=200)
+
+# ---------------------------------------------------------
 
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[UserSchemaBase])
